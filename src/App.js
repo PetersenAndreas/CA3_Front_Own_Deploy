5100763a3c52b27721e56c350b8a249201aa9795
Andreas <andreas.petersen.3540@gmail.com>
2020-04-17 16:37:21 +0200

    Wrote my own frontPage

--- src/App.js
+++ src/App.js
@@ -54,8 +54,18 @@ function App() {
 function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Home - Andreas deployment</h1>
       <p>Home is were the wifi connects automatically</p>
+      <hr/>
+      <p style={{width: 750}}>
+        The frontEnd startcode is easy to use and set up. 
+        The guide helps a lot and the only other thing that could make it more useful/easier to follow is if it had pictures.
+        The same goes for the backEnd startcode, however this a bit more challenging, as Travis is VERY sensitive and 
+        there are so many things that need to work together for it to work properly, but when the startcode is fully set up it works perfectly. <br/>
+        You just have to do it a lot of times to get better at it ¯\_(ツ)_/¯
+
+      </p>
+      <img src="https://media1.tenor.com/images/247d8ee14f4569a7a5d033081ec785d2/tenor.gif" alt="He smash"  style={{width: 520, height: 450}}/>
     </div>
   );
 }
