4ac8aa1a899eecfafaceb528f48c3c201ee990b1
Andreas <andreas.petersen.3540@gmail.com>
2020-04-17 19:37:31 +0200

    Small change to the frontpage

--- src/App.js
+++ src/App.js
@@ -62,9 +62,10 @@ function Home() {
         The guide helps a lot and the only other thing that could make it more useful/easier to follow is if it had pictures.
         The same goes for the backEnd startcode, however this a bit more challenging, as Travis is VERY sensitive and 
         there are so many things that need to work together for it to work properly, but when the startcode is fully set up it works perfectly. <br/>
-        You just have to do it a lot of times to get better at it ¯\_(ツ)_/¯
-
+        You just have to do it a lot of times to get better at it I guess ¯\_(ツ)_/¯
       </p>
+      <a href="https://github.com/PetersenAndreas/CA3-Gruppe3-Front" style={{color: "blue"}}>To the FrontEnd</a> <br/>
+      <a href="https://github.com/PetersenAndreas/CA3-Gruppe3" style={{color: "blue"}}>To the BackEnd</a> <br/>
       <img src="https://media1.tenor.com/images/247d8ee14f4569a7a5d033081ec785d2/tenor.gif" alt="He smash"  style={{width: 520, height: 450}}/>
     </div>
   );
